123e4e26ef75436dd3a64b0053e1c8375175ce66
arjithaaa <62846047+arjithaaa@users.noreply.github.com>
2020-04-19 18:15:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,69 @@
-document.querySelector("#best").innerHTML = "BEST TIME : " + JSON.parse(localStorage.getItem("scores"))[0];
+var no = 1;
+//making the grids
+
+for (var i = 1; i <= 20; i++) {
+  div = document.createElement("div");
+
+  var cont = document.getElementsByClassName("container")[0];
+  cont.appendChild(div);
+  var addAtt = document.querySelectorAll(".container div")[i - 1];
+  addAtt.setAttribute("id", 'div' + (i - 1));
+  addAtt.setAttribute("onclick", 'changeNum(' + (i - 1) + ')');
+}
+
+var checkExist = JSON.parse(localStorage.getItem("scores"));
+if (checkExist == null) document.querySelector("#best").innerHTML = "BEST TIME : 0.000";
+else document.querySelector("#best").innerHTML = "BEST TIME : " + checkExist[0];
 
 //to change the number in each cell when clicked
 
 var a = document.querySelectorAll(".container div");
-function changeNum(i){
+
+function changeNum(i) {
   a[i].classList.add("pressed");
-  setTimeout(function(){
+  setTimeout(function() {
     a[i].classList.remove("pressed");
-  },150);
-  var x = (parseInt(a[i].innerHTML) + 20).toString();
-  if(x<=40)a[i].innerHTML = x;
-  else a[i].innerHTML = "";
-  if(x==60)gameOver();
+  }, 150);
+  if (a[i].innerHTML == no) {
+    var x = (parseInt(a[i].innerHTML) + 20).toString();
+    if (x <= 40) a[i].innerHTML = x;
+    else a[i].innerHTML = "";
+    if (x == 60) gameOver();
+    no++;
+  }
 }
 
 //integer array with no 1-20
 
 var numArray = [];
-for(var i = 1; i<=20;i++)numArray.push(i);
+for (var i = 1; i <= 20; i++) numArray.push(i);
 
 //to generate random index numbers
 
-function genIndex(n){
-  var indexNo = Math.floor(Math.random()*n);
+function genIndex(n) {
+  var indexNo = Math.floor(Math.random() * n);
   return indexNo;
 }
 
 //to generate numbers inside grid
 
 var randomArray = [];
-function genGrid(n){
-  for(var j=0;j<20;j++)
-  {var x=genIndex(n);
-  randomArray.push(numArray[x]);
-  document.getElementById("div"+j).innerHTML = randomArray[j];
-  numArray.splice(x,1);
-  n--;}
+
+function genGrid(n) {
+  for (var j = 0; j < 20; j++) {
+    var x = genIndex(n);
+    randomArray.push(numArray[x]);
+    document.getElementById("div" + j).innerHTML = randomArray[j];
+    numArray.splice(x, 1);
+    n--;
+  }
 }
 
 var inName = document.getElementsByClassName("start")[0];
 
 //to display starting message
 
-function cards(){
+function cards() {
   genGrid(20);
   document.querySelector("#timer").innerHTML = "TIME : 0.000";
   inName.innerHTML = "3";
@@ -50,19 +71,19 @@ function cards(){
   inName.style.fontSize = "700%";
   inName.style.left = "45%";
   inName.style.top = "35%";
-  setTimeout(function(){
+  setTimeout(function() {
     inName.innerHTML = "2";
-  },1000);
+  }, 1000);
 
-  setTimeout(function(){
+  setTimeout(function() {
     inName.innerHTML = "1";
-  },2000);
+  }, 2000);
 
-  setTimeout(function(){
+  setTimeout(function() {
     inName.innerHTML = "";
-    document.querySelector(".container").style.zIndex=10;
+    document.querySelector(".container").style.zIndex = 10;
     triggerTimer();
-  },3000);
+  }, 3000);
 
 }
 
@@ -71,54 +92,55 @@ function cards(){
 //to start timer
 
 var timeToStop;
-function triggerTimer(){
+
+function triggerTimer() {
   var start = Date.now();
- timeToStop = setInterval(function(){
-      document.getElementById("timer").innerHTML = "TIME : " + (parseInt(Date.now()-start)/1000).toString();
-    },1);
-    console.log(start);
-    console.log(timeToStop);
+  timeToStop = setInterval(function() {
+    document.getElementById("timer").innerHTML = "TIME : " + (parseInt(Date.now() - start) / 1000).toString();
+  }, 1);
+  console.log(start);
+  console.log(timeToStop);
 
-    }
+}
 
-  //to end game
+//to end game
 
-function gameOver(){
-    clearInterval(timeToStop);
-    var score = document.getElementById("timer").innerHTML.substring(7,13);
-    document.querySelector(".container").style.zIndex=-1;
-    inName.innerHTML = "Your score is : <strong>" + score + "</strong><br><h3 class='restart'>Click To Restart</h3>";
-    inName.style.fontSize = "200%";
-    inName.style.left = "23%";
-    inName.style.top = "35%";
-    document.getElementById("best").innerHTML = "BEST TIME : " + editLeaderboard(score);
-   }
+function gameOver() {
+  clearInterval(timeToStop);
+  var score = document.getElementById("timer").innerHTML.substring(7, 13);
+  document.querySelector(".container").style.zIndex = -1;
+  inName.innerHTML = "Your score is : <strong>" + score + "</strong><br><h3 class='restart'>Click To Restart</h3>";
+  inName.style.fontSize = "200%";
+  inName.style.left = "23%";
+  inName.style.top = "35%";
+  document.getElementById("best").innerHTML = "BEST TIME : " + editLeaderboard(score);
+}
 
 
 
-function editLeaderboard(s){
-  if(JSON.parse(localStorage.getItem("scores"))==null)var scoreArray=[];
-  else var scoreArray=JSON.parse(localStorage.getItem("scores"));
+function editLeaderboard(s) {
+  if (checkExist == null) var scoreArray = [];
+  else var scoreArray = checkExist;
   console.log(scoreArray);
   scoreArray.push(s);
   scoreArray.sort();
   console.log(scoreArray);
-  if(scoreArray.length == 6)scoreArray.pop();
+  if (scoreArray.length == 6) scoreArray.pop();
   localStorage.setItem("scores", JSON.stringify(scoreArray));
-  var topScores = JSON.parse(localStorage.getItem("scores"));
+  var topScores = checkExist;
   return topScores[0];
 }
 
-function viewLdb(){
-  document.querySelector(".container").style.zIndex=-1;
-  var dispScores = JSON.parse(localStorage.getItem("scores"));
-  inName.innerHTML = "Leaderboard<br>  1)  " + dispScores[0] +"<br>  2)  " + dispScores[1] +"<br>  3)  " + dispScores[2] +"<br>  4)  " + dispScores[3] +"<br>  5)  " + dispScores[4];
+function viewLdb() {
+  document.querySelector(".container").style.zIndex = -1;
+  var dispScores = checkExist;
+  inName.innerHTML = "Leaderboard<br>  1)  " + dispScores[0] + "<br>  2)  " + dispScores[1] + "<br>  3)  " + dispScores[2] + "<br>  4)  " + dispScores[3] + "<br>  5)  " + dispScores[4];
   inName.style.fontSize = "200%";
   inName.style.left = "38%";
   inName.style.top = "25%";
 
-  setTimeout(function(){
-    inName.style.fontSize= "300%";
+  setTimeout(function() {
+    inName.style.fontSize = "300%";
     inName.style.left = "25%";
     inName.style.top = "43%";
     inName.style.color = "green";
